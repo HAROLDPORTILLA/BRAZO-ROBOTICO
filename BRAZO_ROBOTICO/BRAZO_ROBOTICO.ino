670fe96ab70334b171c1700b1f0f16e0b86db6e1
HAROLD <haroldportilla98@gmail.com>
2018-05-04 10:41:25 -0500

    version 0.2

--- BRAZO_ROBOTICO/BRAZO_ROBOTICO.ino
+++ BRAZO_ROBOTICO/BRAZO_ROBOTICO.ino
@@ -19,41 +19,4 @@ void setup() {
   servobase.write(0);
   Serial.begin(9600);
 }
-void loop() {
-Serial.println(servoderecha.read());
-  val = Serial.read();
-    
-if(val=='a'){
-servoderecha.write(0);
-delay(20);}
-  if(val=='q'){
-  servoderecha.write(90);
-  delay(20);}
-    if(val=='w'){
-    servoizquierda.write(0);
-    delay(20);}
-      if(val=='s'){
-      servoizquierda.write(180);
-      delay(20);}
-        if(val=='c'){
-        servobase.write(0);
-        delay(20);}
-          if(val=='z'){
-          servobase.write(180);
-          delay(20);}
-            if(val=='x'){
-            servobase.write(90);
-            delay(20);}
 
-  if(val=='1'){          
-  servogarra.write(10);                 
-  delay(20);
-
-}
-  if(val=='2'){
-    servogarra.write(40);
-    delay(20);
-   
-    }
-
-  }
